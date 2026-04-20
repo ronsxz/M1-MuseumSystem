6977f8458f17d15b011388fcb418beb42def6d46
halcyon-glitch <jamiegayo06@gmail.com>
2026-04-20 01:09:14 +0000

    Implementation of DBconnection

---
+++ DBconnection.java
@@ -0,0 +1,20 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:sqlite:" + System.getProperty("user.dir") + "/museo.db";
+
+    public static Connection connect() {
+        try {
+            Connection conn = DriverManager.getConnection(URL);
+            System.out.println("Connected to DB");
+            return conn;
+        } catch (Exception e) {
+            System.out.println("DB Error: " + e.getMessage());
+            return null;
+        }
+    }
+}
